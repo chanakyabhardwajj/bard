65847cd81ca51877929a4af42473821376169352
Chanakya Bhardwaj <chanakyabhardwajj@gmail.com>
2014-01-23 09:46:13 +0530

    testing stuff

--- public/penman/Articles/ArticlesDirective.js
+++ public/penman/Articles/ArticlesDirective.js
@@ -31,4 +31,54 @@ angular.module('penman.Articles').
                 read(); // initialize
             }
         };
+    }).
+    //source : https://raw2.github.com/thijsw/angular-medium-editor/master/src/angular-medium-editor.js
+    directive('mediumEditor', function() {
+        return {
+            require: 'ngModel',
+            restrict: 'AE',
+            link: function (scope, iElement, iAttrs, ctrl) {
+
+                angular.element(iElement).addClass('angular-medium-editor');
+
+                // Parse options
+                var opts = {};
+                if (iAttrs.options) {
+                    opts = angular.fromJson(iAttrs.options);
+                }
+
+                var placeholder = opts.placeholder || '';
+
+                // view -> model
+                iElement.on('blur', function() {
+                    scope.$apply(function() {
+
+                        // If user cleared the whole text, we have to reset the editor because MediumEditor
+                        // lacks an API method to alter placeholder after initialization
+                        if (iElement.html() === '<p><br></p>') {
+                            opts.placeholder = placeholder;
+                            var editor = new window.MediumEditor(iElement, opts);
+                        }
+
+                        ctrl.$setViewValue(iElement.html());
+                    });
+                });
+
+                // model -> view
+                ctrl.$render = function() {
+                    if (!editor) {
+                        // Hide placeholder when the model is not empty
+                        if (!ctrl.$isEmpty(ctrl.$viewValue)) {
+                            opts.placeholder = '';
+                        }
+
+                        var editor = new window.MediumEditor(iElement, opts);
+                    }
+
+                    iElement.html(ctrl.$isEmpty(ctrl.$viewValue) ? '' : ctrl.$viewValue);
+                };
+
+            }
+        };
+
     });
--- public/penman/modules.js
+++ public/penman/modules.js
@@ -2,7 +2,6 @@
 
 angular.module('penman', ['ngCookies', 'ngResource', 'ngRoute',  'ngSanitize', 'ui.bootstrap', 'ui.route', 'penman.Articles', 'penman.Authors', 'penman.UserStatus']);
 
-//ToDo : Check the following ngResource dependency .. why the fuck is it needed!!
-angular.module('penman.Articles', ['ngResource']);
+angular.module('penman.Articles', []);
 angular.module('penman.Authors', []);
 angular.module('penman.UserStatus', []);
