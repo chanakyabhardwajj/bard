67728b0ac95f6663dfa2525276328dd4064884d5
Chanakya Bhardwaj <chanakyabhardwajj@gmail.com>
2014-01-26 20:10:09 +0530

    auth urls

--- config/env/production.js
+++ config/env/production.js
@@ -8,12 +8,12 @@ module.exports = {
     facebook: {
         clientID: "621077091262153",
         clientSecret: "4f9d9f12c366b6d3fa108a1df1d5cc62",
-        callbackURL: "http://localhost:3000/auth/facebook/callback"
+        callbackURL: "http://sheltered-brook-2177.herokuapp.com/auth/facebook/callback"
     },
     twitter: {
         clientID: "lDr2ruEINuyOMGGVTRWQ",
         clientSecret: "SBUD7jDUOeg8jFUzEfMSSz0qCEydsdvT1kx1zezQw8",
-        callbackURL: "http://localhost:3000/auth/twitter/callback"
+        callbackURL: "http://sheltered-brook-2177.herokuapp.com/auth/twitter/callback"
     },
     github: {
         clientID: "APP_ID",
